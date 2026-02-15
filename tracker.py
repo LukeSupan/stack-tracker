8ca1fc515343f941c5c7498ccfc4a99fdecf7540
Luke <LukeSupan@outlook.com>
2026-02-15 01:36:31 -0500

    newline, that is all

--- tracker.py
+++ tracker.py
@@ -3,9 +3,7 @@
 # if you have some python experience i would recommend doing it.
 # if you need help, ask me, if you have something you think should be added let me know.
 
-
 from games.hero_shooter import run as run_hero_shooter
-
 
 # THIS IS WHERE YOU CHANGE THE FILE NAME!
 with open("games.txt") as f: # (change the name of "games.txt" here to your specific text document)
